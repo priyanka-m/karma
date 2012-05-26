7755ec57c2a83dee0226400198e43ca7038be8fa
root <root@priyanka-laptop.(none)>
2012-05-26 17:18:38 +0530

    Karma is now integrated with Bugzilla

--- start.php
+++ start.php
@@ -1,9 +1,11 @@
 <?php
 function karma_init(){
 		global $CONFIG;
-		add_widget_type('karma',"karma","Find your karma score");
+		add_widget_type('karma',"karma","Find your Karma score");
+		register_entity_type('object','karma');
 	}
 	register_elgg_event_handler('init','system','karma_init'); 
 	register_action("karma/calculate_karma", false, $CONFIG->pluginspath . "karma/actions/calculate_karma.php");
+	
 
 ?>
