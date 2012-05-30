962e411eb31b0eaab890a39ec09695bb56a7d619
root <root@priyanka-laptop.(none)>
2012-05-31 00:06:23 +0530

    Added Cron function

--- start.php
+++ start.php
@@ -1,11 +1,81 @@
 <?php
-function karma_init(){
+	function karma_init(){
 		global $CONFIG;
-		add_widget_type('karma',"karma","Find your Karma score");
-		register_entity_type('object','karma');
+		add_widget_type('karma','karma','Find your Karma score');
+		//register cron hook to trigger function karma_cron weekly 
+		register_plugin_hook('cron','weekly','karma_cron');
+	}
+	//cron function 
+	function karma_cron() {
+		//get all existing users on connect;
+		$entities = get_entities('user');
+		//for each user calculate karma(this is done weekly).
+		foreach ($entities as $entity) {
+			//email of each user 
+			$email = $entity->email;
+			$score  = 0;
+			//call to Bugzilla to return resolved bugs
+			$csv_url = "https://bugzilla.novell.com/buglist.cgi?bug_status=RESOLVED&bug_status=VERIFIED&email1=$email&emailassigned_to1=1&emailinfoprovider1=1&emailtype1=exact&query_format=advanced&title=Bug%20List&ctype=csv";
+			$xmls = file_get_contents($csv_url);
+			$arr = explode("\n",$xmls);
+			array_shift($arr);
+			$max_score = 0;
+			$num_of_bugs = 0;
+			//now for each bug, check its severity.
+			foreach ($arr as $line) {
+				$d = explode(',' ,$line, 8);
+				$severity = $d[1];
+				echo $severity;
+				if (strcasecmp($severity,"Blocker") == 0 ) {
+					$score += 10;
+				} 
+				else if (strcasecmp($severity,"Critical") == 0 ) {
+					$score += 8;
+				}
+				else if (strcasecmp($severity,"Major") == 0 ) {
+					$score += 7;
+				}
+				else if (strcasecmp($severity,"Normal") == 0 ) {
+					$score += 5;
+				}
+				else if (strcasecmp($severity,"Minor") == 0 ) {
+					$score += 3;
+				}
+				else if (strcasecmp($severity,"Enhancement") == 0 ){
+					$score += 2;
+				}
+				$num_of_bugs ++;
+			}
+	
+			/*For now the title Bug Squasher is assigned for securing greater than 85% of maximum
+	 *  	score, Master Ninja for securing greater than 70% but less than 85% and Novice for the rest.*/ 
+			$max_score = $num_of_bugs*10;
+			if ($max_score == 0)
+				$badge = "Novice";
+			else if ($score >= 0.85*$max_score && $score < 0.7*$max_score && $max_score > 0)
+			$badge = "Bug Squasher";
+				else if ($score >=0.7*$max_score && $score < 0.5*$max_score && $max_score > 0)
+			$badge = "Master Ninja";
+			
+			//create an instance ElggObject classs to store karma details for ach user. 
+			$karma = new ElggObject();
+			$guid = $entity->guid;
+			
+			//check if karma object exists for user, if it does then update it.
+			$entities = get_entities('object','karma',$guid);
+			if(isset($entities)) {
+				$karma = $entities[0];
+			}
+			else {
+				$karma->description = "karma score";//TODO:score is not being used, so assign score and work out description.
+				$karma->subtype="karma";
+				$karma->access_id = ACCESS_PUBLIC;
+				$karma->owner_guid = $entity->guid;
+			}
+			$karma->title = $badge;	
+			$karma->save();
+		}//end of foreach user
+		return true;
 	}
 	register_elgg_event_handler('init','system','karma_init'); 
-	register_action("karma/calculate_karma", false, $CONFIG->pluginspath . "karma/actions/calculate_karma.php");
-	
-
 ?>
--- views/default/widgets/karma/view.php
+++ views/default/widgets/karma/view.php
@@ -1,71 +1,33 @@
+<style type="text/css">
+    img.emblem{
+		position:relative;
+		left:30px;
+		height:181px;
+		width:150px;
+	}
+	div.emblem{
+		position:relative;
+		top:-10px;
+	}
+</style>
 <?php 
-	$score  = 0;
-	elgg_extend_view('metatags', 'karma/metatags_ext');
 	
-	//Script that extracts twitter username, blog url, obs repository from Connect.
-	$vars['entity'] = get_entity(get_loggedin_userid());
-	$details = array();
-	foreach($vars['config']->profile as $shortname => $valtype) {
-		$value = $vars['entity']->$shortname;
-		if(strcasecmp($shortname,"twitter") == 0 || strcasecmp($shortname,"obs") == 0 || strcasecmp($shortname,"blog") == 0 )
-		{
-			$details[$shortname] = $value;
-		}
-	}
+	$guid = get_loggedin_userid();
 	
-	//information about resolved bugs from Bugzilla. For each bug fixed 10 points assigned.
-	$email = $vars['entity']->email;
-	$csv_url = "https://bugzilla.novell.com/buglist.cgi?bug_status=RESOLVED&bug_status=VERIFIED&email1=$email&emailassigned_to1=1&emailinfoprovider1=1&emailtype1=exact&query_format=advanced&title=Bug%20List&ctype=csv";
-	$xmls = file_get_contents($csv_url);
-	$arr = explode("\n",$xmls);
-	array_shift($arr);
-	$max_score = 0;
-	$num_of_bugs = 0;
-	foreach ($arr as $line) {
-		$d = explode(',' ,$line, 8);
-		$severity = $d[1];
-		echo $severity;
-		if (strcasecmp($severity,"Blocker") == 0 ) {
-			$score += 10;
-		} 
-		else if (strcasecmp($severity,"Critical") == 0 ) {
-			$score += 8;
-		}
-		else if (strcasecmp($severity,"Major") == 0 ) {
-			$score += 7;
-		}
-		else if (strcasecmp($severity,"Normal") == 0 ) {
-			$score += 5;
-		}
-		else if (strcasecmp($severity,"Minor") == 0 ) {
-			$score += 3;
-		}
-		else if (strcasecmp($severity,"Enhancement") == 0 ){
-			$score += 2;
-		}
-		$num_of_bugs ++;
-	}
+	//get karma instance for loggedin user.
+	$entities = get_entities('object','karma',$guid);
+	$entity = $entities[0];
 	
-	/*For now the title Bug Squasher is assigned for securing greater than 85% of maximum
-	 *  score, Master Ninja for securing greater than 70% but less than 85% and Noob for the rest.*/ 
-	$max_score = $num_of_bugs*10;
-	if ($max_score == 0)
-		$badge = "Noob";
-	else if ($score >= 8.5*$max_score && $score < 7*$max_score && $max_score > 0)
-		$badge = "Bug Squasher";
-	else if ($score >= 7*$max_score && $score < 5*$max_score && $max_score > 0)
-		$badge = "Master Ninja";
-	$img_class = '';		
-	?>
-<br/>
-<div class="usericon">
-	&nbsp;&nbsp;&nbsp;<img <?php echo $img_class; ?> src="<?php echo elgg_format_url($vars['url']."mod/karma/default_icons/".$badge.".jpg");?>" border="0"/>
+	$badge = $entity->title;
+	$img_class = 'class = emblem';
+?>
+<!-- displapy the user's badge  -->
+<div class = "emblem">
+	&nbsp;&nbsp;&nbsp;<img <?php echo $img_class; ?> src="<?php echo elgg_format_url($vars['url']."mod/karma/default_icons/".$badge.".gif");?>" border="0"/>
 </div>
-<br/>
-<div class = "badge">
+<div class = "Score">
 	<?php
-	$title = $badge. " (".$score." Points)";
-	echo elgg_view_title($title);?>
+	echo elgg_view_title($badge);?>
 </div>
 
 
