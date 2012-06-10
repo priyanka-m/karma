17adfc173542cfde4b587d37aac3f070600d7d4d
root <root@priyanka-laptop.(none)>
2012-06-10 22:13:25 +0530

    Made changes to README file.

--- views/default/widgets/karma/edit.php
+++ views/default/widgets/karma/edit.php
@@ -1,13 +1,1 @@
-<?php
-$rss = new SimpleXMLElement('http://planet.opensuse.org/en/rss20.xml', null, true);
-$entities = get_entities('user');
-foreach ($entities as $entity) {
-	$blog = $entity->blog;
-	foreach($rss->xpath('channel/item') as $item)
-	{
-		if(stripos($item->link,$blog) !== False) {
-			$score += 5;
-		}
-	}
-}
-?>
+
